8b60038cb55a1bb82093463593c46fbcce09db9d
cmcaquilala <cmcaquilala@up.edu.ph>
2022-01-21 21:32:13 +0800

    added key

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    private bool hasKey;
+
     private void OnCollisionEnter(Collision collision) {
         if (collision.transform.tag != "Untagged") {
             Debug.Log(collision.transform.tag);
@@ -12,6 +14,15 @@ public class CollisionHandler : MonoBehaviour
 
         if (collision.transform.tag == "Spike") {
             SceneManager.LoadScene("DeadScreen");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Key"))
+        {
+            hasKey = true;
+            Destroy(other.gameObject);
         }
     }
 
